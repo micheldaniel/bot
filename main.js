0a37db9a29a7facf392ea9d9ba3c396135b3d402
michel <micheldaniel2@hotmail.com>
2016-10-23 00:39:33 +0200

    edit main file
    edit main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//load modules
+var express = require('express');
 
+//app
+var app = express();
 
+//load andere codes
+var colorCodes = require('./scripts/colors.js');
+var allebittrex = require('./core/bittrex.js');
 var allepoloniex = require('./core/poloniex.js');
+
+
+allebittrex.alleBittrex();
 allepoloniex.allePoloniex();
+
+
+
+//server
+app.listen(8080, function(){
+   console.log(colorCodes.log()+"Server is opgestart");
+});
